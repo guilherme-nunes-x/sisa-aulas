799358237a0dda295a2dfffc8ab807b9d159c97d
Guilherme nunes <140085055+guilherme-nunes-x@users.noreply.github.com>
2024-05-07 15:10:39 -0300

    Add files via upload

---
+++ guilherme/atv.php
@@ -0,0 +1,69 @@
+<?php
+$aluno1["nome"] = "luiz bernado";
+$aluno1["endereco"] = "Rua A";
+$aluno1["cidade"] = "Vanila";
+$aluno1["estado"] = "Maranhão";
+$aluno1["idade"] = "12";
+$aluno1["cpf"] = "000000000.32";
+
+$aluno2["nome"] = "luwin";
+$aluno2["endereco"] = "Rua b";
+$aluno2["cidade"] = "Vani";
+$aluno2["estado"] = "Marai";
+$aluno2["idade"] = "89";
+$aluno2["cpf"] = "002042032.32";
+
+$aluno3["nome"] = "zeca pagodin";
+$aluno3["endereco"] = "Rua Ae";
+$aluno3["cidade"] = "candede";
+$aluno3["estado"] = "Maracaju";
+$aluno3["idade"] = "80";
+$aluno3["cpf"] = "043028943.21";
+
+$professor["nome"] = "jonh";
+$professor["endereco"] = "Rua Rc";
+$professor["cidade"] = "magalhães";
+$professor["estado"] = "Marianopolis";
+$professor["idade"] = "20";
+$professor["cpf"] = "089043032.32";
+
+$professor2["nome"] = "Raix quadrado";
+$professor2["endereco"] = "Rua TY";
+$professor2["cidade"] = "Vasconcelo";
+$professor2["estado"] = "Pirantinga";
+$professor2["idade"] = "56";
+$professor2["cpf"] = "8909327.32";
+
+$professor3["nome"] = "Jack marrom";
+$professor3["endereco"] = "Rua luiz peixoto";
+$professor3["cidade"] = "jaquele";
+$professor3["estado"] = "Maragantuga";
+$professor3["idade"] = "82";
+$professor3["cpf"] = "980327.32";
+
+$professor4["nome"] = "luiz ternado";
+$professor4["endereco"] = "Rua AH";
+$professor4["cidade"] = "VanilaH";
+$professor4["estado"] = "Marancão";
+$professor4["idade"] = "89";
+$professor4["cpf"] = "048390.32";
+
+echo "nome : ".$aluno1["nome"] ."<br>"."endereco : ". $aluno1["endereco"]."<br>". "cidade : ". $aluno1["cidade"]."<br>". "estado : ". $aluno1["estado"] ."<br>"."idade : ".$aluno1["idade"]  ."<br>"."CPF : ".$aluno1["cpf"];
+echo "<br>";
+echo "<br>";
+echo "nome : ".$aluno2["nome"] ."<br>"."endereco : ". $aluno2["endereco"]."<br>". "cidade : ". $aluno2["cidade"]."<br>". "estado : ". $aluno2["estado"] ."<br>"."idade : ".$aluno2["idade"]  ."<br>"."CPF : ".$aluno2["cpf"];
+echo "<br>";
+echo "<br>";
+echo "nome : ".$aluno3["nome"] ."<br>"."endereco : ". $aluno3["endereco"]."<br>". "cidade : ". $aluno3["cidade"]."<br>". "estado : ". $aluno3["estado"] ."<br>"."idade : ".$aluno3["idade"]  ."<br>"."CPF : ".$aluno3["cpf"];
+echo "<br>";
+echo "<br>";
+echo "nome : ".$professor["nome"] ."<br>"."endereco : ". $professor["endereco"]."<br>". "cidade : ". $professor["cidade"]."<br>". "estado : ". $professor["estado"] ."<br>"."idade : ".$professor["idade"]  ."<br>"."CPF : ".$professor["cpf"];
+echo "<br>";
+echo "<br>";
+echo "nome : ".$professor2["nome"] ."<br>"."endereco : ". $professor2["endereco"]."<br>". "cidade : ". $professor2["cidade"]."<br>". "estado : ". $professor2["estado"] ."<br>"."idade : ".$professor2["idade"]  ."<br>"."CPF : ".$professor2["cpf"];
+echo "<br>";
+echo "<br>";
+echo "nome : ".$professor3["nome"] ."<br>"."endereco : ". $professor3["endereco"]."<br>". "cidade : ". $professor3["cidade"]."<br>". "estado : ". $professor3["estado"] ."<br>"."idade : ".$professor3["idade"]  ."<br>"."CPF : ".$professor3["cpf"];
+echo "<br>";
+echo "<br>";
+echo "nome : ".$professor4["nome"] ."<br>"."endereco : ". $professor4["endereco"]."<br>". "cidade : ". $professor4["cidade"]."<br>". "estado : ". $professor4["estado"] ."<br>"."idade : ".$professor4["idade"]  ."<br>"."CPF : ".$professor4["cpf"];
