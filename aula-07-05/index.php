923361134eb2bdb81a57c613efe04f07513823fd
Guilherme nunes <140085055+guilherme-nunes-x@users.noreply.github.com>
2024-05-14 14:53:27 -0300

    Add files via upload

--- aula-07-05/index.php
+++ aula-07-05/index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../style.css">
     <title>VS</title>
 </head>
 
@@ -15,26 +15,43 @@
     </div>
     <pre>
     <?php
-    $var = array("jonh", "eric", "mander", "erverssom", "jose", "marick", "edmund", "varsen");
-    echo "<h3>for :</h3> <br>";
-    for ($i = 0; $i < 8; $i++) {
-        echo "$i = " . $var[$i] . "<br>";
+    // $var = array("jonh", "eric", "mander", "erverssom", "jose", "marick", "edmund", "varsen");
+    // echo "<h3>for :</h3> <br>";
+    // for ($i = 0; $i < 8; $i++) {
+    //     echo "$i = " . $var[$i] . "<br>";
+    // }
+    // echo "<br> Numero de elemento: " . count($var) . "<br>";
+    // echo "<h2><br>Pre-teste</h2>";
+    // $number = array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+    // echo "<br><h3>for :</h3> <br>";
+    // for ($i = 0; $i < count($number); $i++) {
+    //     echo "$i = " . $number[$i] . "<br>";
+    // }
+    // echo "<br> Numero de elemento: " . count($number) . "<br>";
+
+    echo "<h1>Forseach :</h1><br>";
+    $varl = array("jonh" => 6.1, "eric" => 11, "mander" => 15, "erverssom" => 0, "jose" => 5.3, "marick" => 3, "edmund" => 7, "varisen" => 12, "pedro" => 25);
+    foreach ($varl as $i => $valores) {
+        echo "Alunos: " . $i . " e sua nota " . $valores . "<br>";
     }
-    echo "<br> Numero de elemento: " .count($var) ."<br>";
-    echo "<h2><br>Pre-teste</h2>";
-    $number= array(0,1,2,3,4,5,6,7,8,9,10);
-    echo "<br><h3>for :</h3> <br>";
-    for ($i = 0; $i <count($number); $i++) {
-        echo "$i = " . $number[$i] . "<br>";
-    }
-    echo "<br> Numero de elemento: ".count($number) ."<br>";
-    echo "Forseach :<br>";
-    $var1 = array("jonh" => 12, "eric" => 32, "mander" => 31, "erverssom" => 24, "jose" => 23, "marick" => 45, "edmund" => 56, "var1sen" =>27);
-    foreach ($var1 as $i => $valores) {
-    echo "Os valores são $valores e seus indices $i <br>";
+    echo "<br><br>";
+    echo "<h1>Forseach com if else :</h1> <br><br>";
+    foreach ($varl as $i => $valores) {
+        // echo "Os valores são $valores e seus indices $i <br> <br>";
+        if ($valores <= 10 && $valores > 6) {
+            echo "Aluno aprovado: " . $i . " e sua nota " . $valores . "<br>";
+        } elseif ($valores <= 6  && $valores >= 4) {
+            echo "Aluno recuperacao: " . $i . " e sua nota " . $valores . "<br>";
+        } elseif ($valores < 4) {
+            echo "Aluno reprovado: " . $i . " e sua nota " . $valores . "<br>";
+        } elseif ($valores > 10) {
+            echo "Aluno com nota extra: " . $i . " e sua nota " . $valores . "<br>";
+            echo "pontos extras : ". $valores - 10 ."<br>";
+        }
     }
     ?>
     </pre>
 </body>
 
 </html>
+<h1>
---
+++ aula-14-05/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style.css">
+    <title>VS</title>
+</head>
+
+<body>
+    <div class="sg">
+        <h1>Array:</h1>
+        <h2>definição explicita(sem chave)</h2>
+    </div>
+  
+</body>
+
+</html>
+<h1></h1>
