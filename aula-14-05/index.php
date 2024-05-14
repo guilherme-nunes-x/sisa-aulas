79a8d1b325ced7d82aeaf9e48d36145a5404b234
Guilherme nunes <140085055+guilherme-nunes-x@users.noreply.github.com>
2024-05-14 15:13:11 -0300

    Add files via upload

--- aula-14-05/index.php
+++ aula-14-05/index.php
@@ -13,7 +13,43 @@
         <h1>Array:</h1>
         <h2>definição explicita(sem chave)</h2>
     </div>
-  
+
+    <pre>
+        <?php
+        $var = array(1, 2, 3, 4, 5);
+        echo "Impressao 1 por 1<br>";
+        echo $var[0] . "<br>";
+        echo $var[1] . "<br>";
+        echo $var[2] . "<br>";
+        echo $var[3] . "<br>";
+        echo $var[4] . "<br>";
+        ?>
+    </pre>
+
+    <div class="sg">
+        <h1>Array:</h1>
+        <h2>impressao com For</h2>
+    </div>
+    <pre>
+        <?php
+        for ($i = 0; $i < count($var); $i++) {
+            echo " <br> $i : $var[$i]";
+        }
+        ?>
+    </pre>
+   
+    <div class="sg">
+        <h1>Array:</h1>
+        <h2></h2>
+    </div>
+    <pre>
+        <?php
+        for ($i = 0; $i < count($var); $i++) {
+            echo " <br> $i : $var[$i]";
+        }
+        ?>
+    </pre>
+
 </body>
 
 </html>
