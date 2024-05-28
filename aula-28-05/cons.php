465a10474b1b388dd4dc7d70b042a2f14cb6c04b
Guilherme nunes <guermenunes2@gmail.com>
2024-05-28 15:29:22 -0300

    Add files via upload

--- aula-28-05/cons.php
+++ aula-28-05/cons.php
@@ -15,55 +15,69 @@
 $var = 3.14;
 define("number", $var*3);
 echo number;
-$var1 = 23;
+
 //Valores fixos
 $return = som_text();
 echo "<br>".$return;
 //Valores variaveis
-$return = som($var, $var1);
+$return = som(2, 5);
 echo "<br>"; 
 echo "a soma foi igual ".$return;
 
-$var = 50;
-$var1 = 25;
-
-$return = div($var, $var1);
+$return = div(10, 5);
 echo "<br>";
 echo "a divisão foi igual ".$return;
 
-$var = 46;
-$var1 = 23;
-
-$return = redu($var, $var1);
+$return = redu(4, 10);
 echo "<br>"; 
 echo "a redução foi igual ".$return;
 
-$var = 5;
-$var1 = 20;
-
-$return = multi($var, $var1);
+$return = multi(5, 5);
 echo "<br>"; 
 echo "a multiplicação foi igual ".$return;
 
-$var3 = 32;
-
-$return = multi3($var, $var1, $var3);
+$return = multi3(5, 2, 4);
 echo "<br>"; 
 echo "a multiplicação por três foi igual ".$return;
 
-for($i = 0; $i<100; $i++){
-$var = $i;
-$var1 = $i*2;
 
-$return = som($var, $var1);
+$return = div4(1000, 5, 5,5);
 echo "<br>"; 
-echo "a soma foi igual ".$return;
+echo "a divisão por quatro foi igual ".$return;
 
-}
+$return = soma2(16, 5, 5,5);
+echo "<br>"; 
+echo "a divisão por quatro foi igual ".$return;
+
+// for($i = 0; $i<100; $i++){
+// $var = $i;
+// $var1 = $i*2;
+
+// $return = som($var, $var1);
+// echo "<br>"; 
+// echo "a soma foi igual ".$return;
+
+// }
+$return = sem2(5);
+echo "<br>"; 
+echo "a divisão por quatro foi igual ".$return;
 
 function som($var, $var1){
     $soma = $var + $var1;
     return $soma;
+}
+
+function div4($var, $var1, $var2, $var3){
+    $text_div = $var / $var1 / $var2 / $var3;
+    return $text_div;
+}
+function soma2($var, $var1, $var2, $var3){
+    $text_div = $var + $var1 + $var2 + $var3;
+    return $text_div;
+}
+function sem2($var){
+    $text_div = ($var * $var) + $var /$var;
+    return $text_div;
 }
 function som_text($varl = 2, $varl2 = 4){
     $text_som = $varl + $varl2;
